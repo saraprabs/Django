da7ab709b3e3ca74cacbb1599eb010546a13b6ba
Saranya <vsaranya04@gmail.com>
2026-03-31 12:38:43 +0200

    Add dynamic ALLOWED_HOSTS configuration

--- Employee_portal/settings.py
+++ Employee_portal/settings.py
@@ -9,9 +9,13 @@ https://docs.djangoproject.com/en/1.11/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
-
 import os
+WEBSITE_HOSTNAME = os.environ.get('WEBSITE_HOSTNAME')
 
+if WEBSITE_HOSTNAME:
+    ALLOWED_HOSTS = [WEBSITE_HOSTNAME]
+else:
+    ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
@@ -50,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Employee_portal.urls'

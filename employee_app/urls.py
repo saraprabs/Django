7dd6baf3c7fca96629f9d1d43123e9e33d18af88
Saranya <vsaranya04@gmail.com>
2026-03-31 13:00:24 +0200

    Replace url with path for registration endpoint

--- employee_app/urls.py
+++ employee_app/urls.py
@@ -5,6 +5,6 @@ from employee_app import views
 app_name = 'employee_app'
 
 urlpatterns = [
-    
-    url(r'^register/', views.register, name='register'),
+    # Modern Django 'path' syntax
+    path('register/', views.register, name='register'),
 ]

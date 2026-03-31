5d410a3d4de625204d860c3b6bb2ca314853f780
Saranya <vsaranya04@gmail.com>
2026-03-31 12:59:12 +0200

    Replace url() with path() for URL routing

--- Employee_portal/urls.py
+++ Employee_portal/urls.py
@@ -21,9 +21,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
    
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', views.index, name='index'),
-    url(r'^employee_app/',include('employee_app.urls')),
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'), # No more regex r'^$'
+    path('employee_app/', include('employee_app.urls')),
 ]
 
 if settings.DEBUG:

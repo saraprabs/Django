dec60aa29907ba46085e092e230b4ff08aef28ad
Saranya <vsaranya04@gmail.com>
2026-04-07 13:28:22 +0200

    Update CSRF_TRUSTED_ORIGINS for new domain

--- Employee_portal/settings.py
+++ Employee_portal/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.11/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
-CSRF_TRUSTED_ORIGINS = ['https://mydjangowebapp-eagecpf9a2hhgyfn.westeurope-01.azurewebsites.net']
+CSRF_TRUSTED_ORIGINS = ['https://mydjangowebappemployeeportal-gpc9eadbhpcue9bw.westeurope-01.azurewebsites.net',]
 import os
 WEBSITE_HOSTNAME = os.environ.get('WEBSITE_HOSTNAME')
 
